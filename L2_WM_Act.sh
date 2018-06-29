103f582b51bc8a503ff62898e0c19e3859d6d1f6
DVSneuro <david.v.smith@temple.edu>
2018-06-29 15:16:27 -0400

    initial commit

---
+++ L2_WM_Act.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+basedir=`pwd`
+MAINDATADIR=${basedir}/data
+MAINOUTPUTDIR=${basedir}/fsl
+
+##bash L2_WM_Act.sh $subj $task $run
+subj=$1
+
+INPUT01=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/tfMRI_WM_LR/L1_WM_Act.feat
+INPUT02=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/tfMRI_WM_RL/L1_WM_Act.feat
+OUTPUT=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/L2_WM_Act
+
+# checking L2 output
+NCOPES=2 #check last cope since they are done sequentially
+if [ -e ${OUTPUT}.gfeat/cope${NCOPES}.feat/cluster_mask_zstat1.nii.gz ]; then
+  exit
+else
+  rm -rf ${OUTPUT}.gfeat
+fi
+
+
+#find and replace
+ITEMPLATE=${BASEDIR}/templates/L2_WM_Act.fsf
+OTEMPLATE=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/L2_WM_Act.fsf
+sed -e 's@OUTPUT@'$OUTPUT'@g' \
+-e 's@INPUT01@'$INPUT01'@g' \
+-e 's@INPUT02@'$INPUT02'@g' \
+<$ITEMPLATE> $OTEMPLATE
+
+#runs feat on output template
+feat $OTEMPLATE
+
+for C in `seq $NCOPES`; do
+  rm -rf ${OUTPUT}.gfeat/cope${C}.feat/filtered_func_data.nii.gz
+  rm -rf ${OUTPUT}.gfeat/cope${C}.feat/var_filtered_func_data.nii.gz
+done

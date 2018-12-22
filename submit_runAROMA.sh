b579085df3ac96b3e4acba0582594e105e85d351
DVSneuro <david.v.smith@temple.edu>
2018-12-22 01:41:46 -0500

    change bg pct to 1 and rename

--- submit_runAROMA.sh
+++ submit_runAROMA.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-echo "sleeping for 2 hours"
-sleep 2h
-
 for task in WM SOCIAL EMOTION GAMBLING; do
   for subj in `cat EuniceSubs.txt`; do
   	for RUN in LR RL; do
 
   		#Manages the number of jobs and cores
-  		SCRIPTNAME=runAROMA.sh
+  		SCRIPTNAME=runAROMA_hcp.sh
   		NCORES=8
   		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
   	  		sleep 1m
   		done
-  		bash runAROMA.sh $task $RUN $subj &
+  		bash runAROMA_hcp.sh $task $RUN $subj &
   		sleep 5s
 
   	done

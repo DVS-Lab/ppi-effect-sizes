4d4bbe11d8672c41eb1b443a511898f6eb19b22e
DVSneuro <david.v.smith@temple.edu>
2018-12-24 12:35:14 -0500

    update sublists and loops

--- submit_runAROMA.sh
+++ submit_runAROMA.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for task in WM SOCIAL EMOTION GAMBLING; do
+for task in GAMBLING WM SOCIAL EMOTION; do
   for subj in `cat EuniceSubs.txt`; do
   	for RUN in LR RL; do
 
   		#Manages the number of jobs and cores
   		SCRIPTNAME=runAROMA_hcp.sh
-  		NCORES=8
+  		NCORES=18
   		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
   	  		sleep 1m
   		done

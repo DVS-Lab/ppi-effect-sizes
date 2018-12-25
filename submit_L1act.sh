36ddc803fd44623668d942eeacabc4b46aec20c4
DVSneuro <david.v.smith@temple.edu>
2018-12-25 13:30:18 -0500

    update sublist and set brain/bg to 1pct

--- submit_L1act.sh
+++ submit_L1act.sh
@@ -2,17 +2,17 @@
 
 
 for task in Soc Gam WM Emo; do
-  for subj in `cat Michelle_AllSubs_n146.txt`; do
+  for subj in `cat EuniceSubs.txt`; do
   	for RUN in LR RL; do
 
   		#Manages the number of jobs and cores
   		SCRIPTNAME=L1_${task}_Act.sh
-  		NCORES=10
+  		NCORES=48
   		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
   	  		sleep 1m
   		done
   		bash $SCRIPTNAME $RUN $subj &
-  		sleep 5s
+  		sleep 1s
 
   	done
   done

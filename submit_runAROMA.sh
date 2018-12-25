7c6b20230dc9a69b04432e5d50abedb2e349bdf9
DVSneuro <david.v.smith@temple.edu>
2018-12-24 21:45:27 -0500

    update cores

--- submit_runAROMA.sh
+++ submit_runAROMA.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for task in EMOTION GAMBLING WM SOCIAL; do
+for task in SOCIAL EMOTION GAMBLING WM; do
   for subj in `cat EuniceSubs.txt`; do
   	for RUN in LR RL; do
 
   		#Manages the number of jobs and cores
   		SCRIPTNAME=runAROMA_hcp.sh
-  		NCORES=32
+  		NCORES=48
   		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
   	  		sleep 1m
   		done

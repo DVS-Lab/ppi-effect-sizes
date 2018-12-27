a261f4ab96a93a0a2093dff79dd1e15269df9fc0
DVSneuro <david.v.smith@temple.edu>
2018-12-27 09:40:54 -0500

    update sub lists

--- submit_L1act.sh
+++ submit_L1act.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-for task in Soc Gam WM Emo; do
+for task in Gam WM Emo Soc; do
   for subj in `cat EuniceSubs.txt`; do
   	for RUN in LR RL; do
 
   		#Manages the number of jobs and cores
   		SCRIPTNAME=L1_${task}_Act.sh
-  		NCORES=40
+  		NCORES=42
   		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
   	  		sleep 1m
   		done
--- submit_L2act.sh
+++ submit_L2act.sh
@@ -16,19 +16,3 @@ for task in Emo Gam WM Soc; do
   done
 done
 
-
-for task in WM Gam; do
-  for subj in `cat sublist`; do
-
-    #Manages the number of jobs and cores
-    SCRIPTNAME=L2_${task}_Act.sh
-    NCORES=12
-    while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-      sleep 1m
-    done
-    bash $SCRIPTNAME $subj &
-    sleep 5s
-
-  done
-done
-

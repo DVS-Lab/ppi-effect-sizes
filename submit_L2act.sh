5eb924450d2bffbc53dc101a3079623b02adf8fb
DVSneuro <david.v.smith@temple.edu>
2018-12-26 19:50:21 -0500

    update sublist

--- submit_L2act.sh
+++ submit_L2act.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-for task in WM Gam; do
-  for subj in `cat Michelle_AllSubs_n146.txt`; do
+for task in Emo Gam WM Soc; do
+  for subj in `cat EuniceSubs.txt`; do
 
     #Manages the number of jobs and cores
     SCRIPTNAME=L2_${task}_Act.sh
-    NCORES=12
+    NCORES=40
     while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
       sleep 1m
     done
     bash $SCRIPTNAME $subj &
-    sleep 5s
+    sleep 1s
 
   done
 done

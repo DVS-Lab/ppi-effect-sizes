30f5545aab539fe807042bbabd34396620ecb638
DVSneuro <david.v.smith@temple.edu>
2018-12-31 01:11:17 -0500

    update loops

--- submit_L2ppi.sh
+++ submit_L2ppi.sh
@@ -2,17 +2,17 @@
 
 
 
-for task in Soc; do
-  for subj in `cat sublist`; do
+for task in Gam WM Emo Soc; do
+  for subj in `cat EuniceSubs.txt`; do
 
     #Manages the number of jobs and cores
     SCRIPTNAME=L2_${task}_PPI.sh
-    NCORES=12
+    NCORES=42
     while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
       sleep 1m
     done
     bash $SCRIPTNAME $subj &
-    sleep 5s
+    sleep 1s
 
   done
 done

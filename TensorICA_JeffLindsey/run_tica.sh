cc7189ff045f2530adc2b992ab0488f296f950d8
DVSneuro <david.v.smith@temple.edu>
2020-11-27 21:22:21 -0500

    fix paths

--- TensorICA_JeffLindsey/run_tica.sh
+++ TensorICA_JeffLindsey/run_tica.sh
@@ -6,7 +6,7 @@ cd fsl
 for task in Soc Gam; do
   for run in LR RL; do
 
-    melodic -i ${basedir}/LindseySubs_task-${task}_run-${run}_paths.txt \
+    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths.txt \
     -o tica_00dim_task-${task}_run-${run}_LindseySubs.ica -v --nobet --bgimage=mean_func \
     --report --guireport=report.html -d 0 \
     --mmthresh=0.5 --Ostats -a tica

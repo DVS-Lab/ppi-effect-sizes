148a4eeef67ee631cb2c9980188f6152a0152b95
DVSneuro <david.v.smith@temple.edu>
2020-11-27 21:20:03 -0500

    update paths

--- TensorICA_JeffLindsey/run_tica.sh
+++ TensorICA_JeffLindsey/run_tica.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-for 
-melodic -i filelist_social_RL.txt -o tica_00dim_social_RL_WB.ica -v --nobet --bgimage=mean_func --report --guireport=report.html -d 0 --mmthresh=0.5 --Ostats -a tica
-melodic -i filelist_social_LR.txt -o tica_00dim_social_LR_WB.ica -v --nobet --bgimage=mean_func --report --guireport=report.html -d 0 --mmthresh=0.5 --Ostats -a tica
+cd ..
+basedir=`pwd`
+cd fsl
+for task in Soc Gam; do
+  for run in LR RL; do
 
-melodic -i filelist_gambling_RL.txt -o tica_00dim_gambling_RL_WB.ica -v --nobet --bgimage=mean_func --report --guireport=report.html -d 0 --mmthresh=0.5 --Ostats -a tica
-melodic -i filelist_gambling_LR.txt -o tica_00dim_gambling_LR_WB.ica -v --nobet --bgimage=mean_func --report --guireport=report.html -d 0 --mmthresh=0.5 --Ostats -a tica
+    melodic -i ${basedir}/LindseySubs_task-${task}_run-${run}_paths.txt \
+    -o tica_00dim_task-${task}_run-${run}_LindseySubs.ica -v --nobet --bgimage=mean_func \
+    --report --guireport=report.html -d 0 \
+    --mmthresh=0.5 --Ostats -a tica
 
-LindseySubs_task-Soc_run-LR_paths.txt
+  done
+done

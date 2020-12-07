caf04063bc56ce42248f11fcc5e5e05d98549f48
DVSneuro <david.v.smith@temple.edu>
2020-12-07 01:33:09 -0500

    update paths for new L3 templates with motion covs

--- TensorICA_JeffLindsey/run_tica.sh
+++ TensorICA_JeffLindsey/run_tica.sh
@@ -13,15 +13,15 @@ for task in Soc Gam; do
   for run in LR RL; do
 
     melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths.txt \
-    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs.ica -v --nobet --bgimage=mean_func \
+    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new.ica -v --nobet --bgimage=mean_func \
     --report --guireport=report.html -d 0 \
     --Tdes=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.mat --Tcon=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.con \
-    --Sdes=${basedir}/TensorICA_JeffLindsey/L3_n279_withCovs.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_n279_withCovs.con \
+    --Sdes=${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con \
     --mmthresh=0.5 --Ostats -a tica
 
 
     melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths.txt \
-    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns.ica -v --nobet --bgimage=mean_func \
+    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new.ica -v --nobet --bgimage=mean_func \
     --report --guireport=report.html -d 0 \
     --Tdes=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.mat --Tcon=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.con \
     --Sdes=${basedir}/TensorICA_JeffLindsey/L3_n279.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_n279.con \

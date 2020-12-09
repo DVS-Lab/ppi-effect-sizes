65ab5a85e8c7d13fae27334034fe7a944576df0b
DVSneuro <david.v.smith@temple.edu>
2020-12-09 16:52:15 -0500

    initial commit

---
+++ AROMAregress.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sub=$1
+task=$2
+run=$3
+
+# input data
+hcpdatadir=/data/projects/human-connectome-project-openaccess/HCP1200
+indata=${hcpdatadir}/${sub}/MNINonLinear/Results/tfMRI_${task}_${run}/tfMRI_${task}_${run}.nii.gz
+
+# project dir (outputs and some inputs will be beneath here)
+ppidir=/data/projects/ppi-effect-sizes
+
+# aroma stuff
+aromadir=${ppidir}/fsl/100307/MNINonLinear/Results/tfMRI_${task}_${run}/smoothing.feat/ICA_AROMA
+mask=${aromadir}/mask.nii.gz
+badICs=${aromadir}/ICA_AROMA/classified_motion_ICs.txt
+melodic_mix=${aromadir}/ICA_AROMA/melodic.ica/melodic_mix
+outdata=${aromadir}/ICA_AROMA/denoised_func_data_nonaggr_nosmoothing
+
+# regress out aroma components 
+if [ ! -e $outdata ]; then
+	echo "denoising $indata"
+	fsl_regfilt -i $indata \
+	    -f $(cat ${badICs}) \
+	    -d $melodic_mix \
+	    -o $outdata \
+      -m $mask
+fi
---
+++ run_AROMAregress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for subj in `cat TensorICA_JeffLindsey/LindseySubs.txt`; do
+  for task in GAMBLING SOCIAL; do
+  	for run in LR RL; do
+
+  		#Manages the number of jobs and cores
+  		SCRIPTNAME=AROMAregress.sh
+  		NCORES=25
+  		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+  	  		sleep 1m
+  		done
+  		bash $SCRIPTNAME $subj $task $run &
+  		sleep 1s
+
+  	done
+  done
+done

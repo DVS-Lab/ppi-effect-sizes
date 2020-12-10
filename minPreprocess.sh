d16f66da25f5ecb5b93f07cd9501ea8ef826df32
DVSneuro <david.v.smith@temple.edu>
2020-12-09 22:50:46 -0500

    use sub instead of subj

--- minPreprocess.sh
+++ minPreprocess.sh
@@ -7,7 +7,7 @@ task=$2
 run=$3
 
 # setting up dirs
-mainoutput=${basedir}/fsl/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}
+mainoutput=${basedir}/fsl/${sub}/MNINonLinear/Results/tfMRI_${task}_${run}
 output=${mainoutput}/preprocess_sub-${sub}_task-${task}_run-${run}_hp-200_sm-0
 data=${mainoutput}/smoothing.feat/ICA_AROMA/denoised_func_data_nonaggr_nosmoothing.nii.gz
 nvols=`fslnvols ${data}`
@@ -17,7 +17,7 @@ if [ -e ${output}.feat/filtered_func_data.nii.gz ]; then
   echo "output exists, skipping...."
   exit
 else
-	echo "re-running $subj $task $run" >> re-run_preprocess.log
+	echo "re-running $sub $task $run" >> re-run_preprocess.log
   rm -rf ${output}.feat
 fi
 

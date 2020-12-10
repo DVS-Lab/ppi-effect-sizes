6009d5e77b191da41e4fb92b5a796f46ad4b65dc
DVSneuro <david.v.smith@temple.edu>
2020-12-10 15:30:03 -0500

    unsmoothed data

--- TensorICA_JeffLindsey/make_filelists.sh
+++ TensorICA_JeffLindsey/make_filelists.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 basedir=/data/projects/ppi-effect-sizes/fsl
-rm -rf LindseySubs_task-*paths.txt
+rm -rf LindseySubs_task-*paths_unsmoothed.txt
 for run in LR RL; do
   for s in `cat LindseySubs.txt`; do
-    fname_gam=${basedir}/${s}/MNINonLinear/Results/tfMRI_GAMBLING_${run}/L1_Gam_Act.feat/filtered_func_data.nii.gz
-    echo $fname_gam >> LindseySubs_task-Gam_run-${run}_paths.txt
-    fname_soc=${basedir}/${s}/MNINonLinear/Results/tfMRI_SOCIAL_${run}/L1_Social_Act.feat/filtered_func_data.nii.gz
-    echo $fname_soc >> LindseySubs_task-Soc_run-${run}_paths.txt
+    fname_gam=${basedir}/${s}/MNINonLinear/Results/tfMRI_GAMBLING_${run}/preprocess_sub-${sub}_task-${task}_run-${run}_hp-200_sm-0.feat/filtered_func_data.nii.gz
+    echo $fname_gam >> LindseySubs_task-Gam_run-${run}_paths_unsmoothed.txt
+    fname_soc=${basedir}/${s}/MNINonLinear/Results/tfMRI_SOCIAL_${run}/preprocess_sub-${sub}_task-${task}_run-${run}_hp-200_sm-0.feat/filtered_func_data.nii.gz
+    echo $fname_soc >> LindseySubs_task-Soc_run-${run}_paths_unsmoothed.txt
   done
 done
--- TensorICA_JeffLindsey/run_tica.sh
+++ TensorICA_JeffLindsey/run_tica.sh
@@ -12,16 +12,16 @@ cd fsl
 for task in Soc Gam; do
   for run in LR RL; do
 
-    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths.txt \
-    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new.ica -v --nobet --bgimage=mean_func \
+    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt \
+    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed.ica -v --nobet --bgimage=mean_func \
     --report --guireport=report.html -d 0 \
     --Tdes=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.mat --Tcon=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.con \
     --Sdes=${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con \
     --mmthresh=0.5 --Ostats -a tica
 
 
-    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths.txt \
-    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new.ica -v --nobet --bgimage=mean_func \
+    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt \
+    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed.ica -v --nobet --bgimage=mean_func \
     --report --guireport=report.html -d 0 \
     --Tdes=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.mat --Tcon=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.con \
     --Sdes=${basedir}/TensorICA_JeffLindsey/L3_n279.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_n279.con \

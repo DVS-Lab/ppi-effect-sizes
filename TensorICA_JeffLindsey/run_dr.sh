4d50ad25edb0d362dd66b054725f1e1749fceb14
DVSneuro <david.v.smith@temple.edu>
2020-12-12 01:31:36 -0500

    fix path error

--- TensorICA_JeffLindsey/run_dr.sh
+++ TensorICA_JeffLindsey/run_dr.sh
@@ -18,6 +18,6 @@ dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_ne
 
 rm -rf tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-  1 ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con 1 0 \
+  1 ${basedir}/TensorICA_JeffLindsey/L3_n279.mat ${basedir}/TensorICA_JeffLindsey/L3_n279.con 1 0 \
   tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression \
   `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`

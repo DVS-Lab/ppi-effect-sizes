2b9a805d5223316b5fae806c2ba48ed7d1c1db2a
DVSneuro <david.v.smith@temple.edu>
2020-12-12 01:15:36 -0500

    move comments

--- TensorICA_JeffLindsey/run_dr.sh
+++ TensorICA_JeffLindsey/run_dr.sh
@@ -7,14 +7,17 @@ cd ..
 basedir=`pwd`
 cd fsl
 
+# Usage: dual_regression <group_IC_maps> <des_norm> <design.mat> <design.con> <n_perm> [--thr] <output_directory> <input1> <input2> <input3> .........
+# e.g.   dual_regression groupICA.gica/groupmelodic.ica/melodic_IC 1 design.mat design.con 500 0 grot `cat groupICA.gica/.filelist`
+
 
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-1 -1 1 0 \ # <des_norm> <design.mat> <design.con> <n_perm> [--thr] (note: -1 sets "<design.mat> <design.con>" to one sample t-test)
+1 -1 1 0 \
 tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/dual_regression \
 `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`
 
 
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-1 -1 1 0 \ # <des_norm> <design.mat> <design.con> <n_perm> [--thr] (note: -1 sets "<design.mat> <design.con>" to one sample t-test)
+1 -1 1 0 \
 tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression \
 `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`

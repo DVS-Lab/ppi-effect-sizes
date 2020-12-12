2df026b6c35ff0dd6375c4c61e22cfb5cfa2f7f1
DVSneuro <david.v.smith@temple.edu>
2020-12-12 10:07:05 -0500

    removing --thr option since it is interferring with output

--- TensorICA_JeffLindsey/run_dr.sh
+++ TensorICA_JeffLindsey/run_dr.sh
@@ -12,12 +12,12 @@ cd fsl
 
 rm -rf tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/dual_regression
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-  1 ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con 1 0 \
+  1 ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con 1 \
   tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/dual_regression \
   `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`
 
 rm -rf tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-  1 ${basedir}/TensorICA_JeffLindsey/L3_n279.mat ${basedir}/TensorICA_JeffLindsey/L3_n279.con 1 0 \
+  1 ${basedir}/TensorICA_JeffLindsey/L3_n279.mat ${basedir}/TensorICA_JeffLindsey/L3_n279.con 1 \
   tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression \
   `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`

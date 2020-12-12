364e1e740f6d8cc1b5937238e38bffd9e614f878
DVSneuro <david.v.smith@temple.edu>
2020-12-12 01:26:43 -0500

    change paths and add des and con

--- TensorICA_JeffLindsey/run_dr.sh
+++ TensorICA_JeffLindsey/run_dr.sh
@@ -10,14 +10,14 @@ cd fsl
 # Usage: dual_regression <group_IC_maps> <des_norm> <design.mat> <design.con> <n_perm> [--thr] <output_directory> <input1> <input2> <input3> .........
 # e.g.   dual_regression groupICA.gica/groupmelodic.ica/melodic_IC 1 design.mat design.con 500 0 grot `cat groupICA.gica/.filelist`
 
-
+rm -rf tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/dual_regression
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-1 -1 1 0 \
-tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/dual_regression \
-`cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`
+  1 ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}.mat ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}.con 1 0 \
+  tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/dual_regression \
+  `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`
 
-
+rm -rf tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression
 dual_regression tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed-concat.ica/melodic_IC \
-1 -1 1 0 \
-tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression \
-`cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`
+  1 ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat ${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con 1 0 \
+  tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed-concat.ica/dual_regression \
+  `cat ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt`

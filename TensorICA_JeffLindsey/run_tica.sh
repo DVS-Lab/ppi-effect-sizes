2e28ab2ca8ccb8ed42612ff1a291807e10ebb6a4
DVSneuro <david.v.smith@temple.edu>
2021-05-14 09:10:58 -0400

    update paths for merged tica

--- TensorICA_JeffLindsey/run_tica.sh
+++ TensorICA_JeffLindsey/run_tica.sh
@@ -9,24 +9,15 @@ cd ..
 basedir=`pwd`
 cd fsl
 
-for task in Soc Gam; do
-  for run in LR RL; do
 
-    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt \
-    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_wCovs_new_unsmoothed.ica -v --nobet --bgimage=mean_func \
-    --report --guireport=report.html -d 0 \
-    --Tdes=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.mat --Tcon=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.con \
-    --Sdes=${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_task-${task}_run-${run}_withCovs.con \
-    --mmthresh=0.5 --Ostats -a tica
+ melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-Soc_paths_unsmoothed.txt \
+ -o tica_00dim_task-Soc_LindseySubs_unsmoothed_merged.ica -v --nobet --bgimage=mean_func \
+ --report --guireport=report.html -d 0 \
+ --mmthresh=0.5 --Ostats -a tica &
 
 
-    melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-${task}_run-${run}_paths_unsmoothed.txt \
-    -o tica_00dim_task-${task}_run-${run}_LindseySubs_wDesigns_new_unsmoothed.ica -v --nobet --bgimage=mean_func \
-    --report --guireport=report.html -d 0 \
-    --Tdes=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.mat --Tcon=${basedir}/TensorICA_JeffLindsey/L1_task-${task}_run-${run}_design.con \
-    --Sdes=${basedir}/TensorICA_JeffLindsey/L3_n279.mat --Scon=${basedir}/TensorICA_JeffLindsey/L3_n279.con \
-    --mmthresh=0.5 --Ostats -a tica
+ melodic -i ${basedir}/TensorICA_JeffLindsey/LindseySubs_task-Gam_paths_unsmoothed.txt \
+ -o tica_00dim_task-Gam_LindseySubs_unsmoothed_merged.ica -v --nobet --bgimage=mean_func \
+ --report --guireport=report.html -d 0 \
+ --mmthresh=0.5 --Ostats -a tica
 
-
-  done
-done
